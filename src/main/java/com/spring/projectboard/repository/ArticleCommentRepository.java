2327953951840aced1fbefe729894ea7fcf498b6
JadeKim042386 <jk042386@gmail.com>
2023-07-05 23:38:33 +0900

    feat: Data REST 설정 - 게시글, 댓글의 json api를 자동으로 restful하게 만듬 #1

--- src/main/java/com/spring/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/spring/projectboard/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.spring.projectboard.repository;
 
 import com.spring.projectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- src/main/java/com/spring/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/spring/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.spring.projectboard.repository;
 
 import com.spring.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
