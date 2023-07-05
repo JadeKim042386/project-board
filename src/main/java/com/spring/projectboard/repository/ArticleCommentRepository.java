418034d1898a4467d600218f041c9e83483c7383
JadeKim042386 <jk042386@gmail.com>
2023-07-05 22:03:19 +0900

    feat: repository interface 작성 - DB에 접근 #10

---
+++ src/main/java/com/spring/projectboard/repository/ArticleCommentRepository.java
@@ -0,0 +1,7 @@
+package com.spring.projectboard.repository;
+
+import com.spring.projectboard.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+}
