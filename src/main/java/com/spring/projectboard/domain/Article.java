f734184ad11d2067b8bee09116ef8317e6c3bc72
JadeKim042386 <jk042386@gmail.com>
2023-07-05 22:56:17 +0900

    feat: AuditingFields 추출 - createdBy, createdAt, modifiedBy, modifiedAt은 엔티티에 반복적으로 들어가고 도메인과 직접적으로 연관이 없으므로 추출이 가능 - '@MappedSuperclass'를 사용하여 상속 - '@DateTimeFormat' 추가 및 옵션 개선 #12

--- src/main/java/com/spring/projectboard/domain/Article.java
+++ src/main/java/com/spring/projectboard/domain/Article.java
@@ -23,9 +23,9 @@ import java.util.Set;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
-@EntityListeners(AuditingEntityListener.class)
+
 @Entity
-public class Article {
+public class Article extends AuditingFields{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //MySQL에서는 IDENTITY 방식으로 auto-increase
     private Long id;
@@ -35,16 +35,10 @@ public class Article {
 
     @Setter private String hashtag; //해시태그
 
-
     @ToString.Exclude
     @OrderBy("id")
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
-
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; //생성일시
-    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; //생성자
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; //수정일시
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; //수정자
 
     protected Article() {
     }
