2b33f0963bf1d6836461edf3c9315e7d6e86fa76
JadeKim042386 <jk042386@gmail.com>
2023-07-09 17:20:44 +0900

    feat: 게시글 서비스 로직 테스트 코드 업데이 & DTO 설계 & 쿼리 메소드 작성 - 게시글 서비스 로직 테스트 정의 - Mockito를 사용하여 테스트 작성 - 테스트에 필요한 repository 쿼리 메소드 추가

---
+++ src/main/java/com/spring/projectboard/dto/ArticleWithCommentsDto.java
@@ -0,0 +1,24 @@
+package com.spring.projectboard.dto;
+
+import com.spring.projectboard.domain.UserAccount;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public record ArticleWithCommentsDto(
+        Long id,
+        String title,
+        String content,
+        String hashtag,
+        UserAccount userAccount,
+        Set<ArticleCommentDto> articleCommentDtos,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
+
+) {
+    public static ArticleWithCommentsDto of(Long id, String title, String content, String hashtag, UserAccount userAccount, Set<ArticleCommentDto> articleCommentDtos, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleWithCommentsDto(id, title, content, hashtag, userAccount, articleCommentDtos, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+}
---
+++ src/main/java/com/spring/projectboard/service/ArticleService.java
@@ -0,0 +1,36 @@
+package com.spring.projectboard.service;
+
+import com.spring.projectboard.domain.Article;
+import com.spring.projectboard.domain.constant.SearchType;
+import com.spring.projectboard.dto.ArticleDto;
+import com.spring.projectboard.dto.ArticleWithCommentsDto;
+import com.spring.projectboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    public List<Article> searchArticles(SearchType searchType, String title, Pageable pageable) {
+        return List.of();
+    }
+
+    public ArticleWithCommentsDto getArticle(Long articleId) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+    }
+
+    public void updateArticle(ArticleDto articleDto) {
+    }
+}
