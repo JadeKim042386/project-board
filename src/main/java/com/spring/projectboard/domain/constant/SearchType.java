912ceac428f33da8500566e7b89a27a3f0f9214e
JadeKim042386 <jk042386@gmail.com>
2023-07-11 02:15:51 +0900

    feat: 검색 기능을 위해 SearchType 옵션 전달 & 테스트 업데이트 - searchType의 드롭 다운 리스트는 thymeleaf로 전달받아 사용 - 테스트 추가 #37

--- src/main/java/com/spring/projectboard/domain/constant/SearchType.java
+++ src/main/java/com/spring/projectboard/domain/constant/SearchType.java
@@ -1,5 +1,17 @@
 package com.spring.projectboard.domain.constant;
 
+import lombok.Getter;
+
+@Getter
 public enum SearchType {
-    TITLE, CONTENT, HASHTAG, ID, NICKNAME
+    TITLE("제목"),
+    CONTENT("본문"),
+    HASHTAG("해시태그"),
+    ID("유저 ID"),
+    NICKNAME("닉네임");
+    private final String description;
+
+    SearchType(String description) {
+        this.description = description;
+    }
 }
