04f3b81c57beabb05ffc2fdbd41a640149b9b1ad
JadeKim042386 <jk042386@gmail.com>
2023-07-11 22:42:56 +0900

    refactor: UserAccountDto에 메타 정보를 포함하지 않는 `of()` 팩토리 메소드 추가 - 테스트에서 useraccount에 대한 메타 정보는 없기때문에 입력하지 않도록 하기 위함 - 글쓰기, 수정에서도 일단 메타 정보를 입력하지 않아도 되도록 수정

--- src/main/java/com/spring/projectboard/controller/ArticleController.java
+++ src/main/java/com/spring/projectboard/controller/ArticleController.java
@@ -96,11 +96,7 @@ public class ArticleController {
                                 "pw",
                                 "joo@gmail.com",
                                 "Joo",
-                                "memo",
-                                null,
-                                null,
-                                null,
-                                null)
+                                "memo")
                 )
         );
         return "redirect:/articles";
@@ -127,11 +123,7 @@ public class ArticleController {
                                 "pw",
                                 "joo@gmail.com",
                                 "Joo",
-                                "memo",
-                                null,
-                                null,
-                                null,
-                                null)
+                                "memo")
                 )
         );
         return "redirect:/articles/" + articleId;
