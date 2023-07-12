7e2bc7373fa238069d2136d2debfb9c8dd64f92a
JadeKim042386 <jk042386@gmail.com>
2023-07-12 09:47:39 +0900

    feat: 작성자와 수정자가 일치하는지 확인하기위해 `userId` 추가 - 게시글/댓글의 등록, 수정, 삭제 버튼이 각각 작성자와 로그인한 사용자와 일치해야만 보일 수 있도록 `Response`정보에 `userId`를 추가

--- src/main/java/com/spring/projectboard/dto/response/ArticleWithCommentResponse.java
+++ src/main/java/com/spring/projectboard/dto/response/ArticleWithCommentResponse.java
@@ -15,10 +15,11 @@ public record ArticleWithCommentResponse(
         LocalDateTime createdAt,
         String email,
         String nickname,
+        String userId,
         Set<ArticleCommentResponse> articleCommentResponses
 ) {
-    public static ArticleWithCommentResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponse> articleCommentResponses) {
-        return new ArticleWithCommentResponse(id, title, content, hashtag, createdAt, email, nickname, articleCommentResponses);
+    public static ArticleWithCommentResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, String userId, Set<ArticleCommentResponse> articleCommentResponses) {
+        return new ArticleWithCommentResponse(id, title, content, hashtag, createdAt, email, nickname, userId, articleCommentResponses);
     }
 
     public static ArticleWithCommentResponse from(ArticleWithCommentsDto dto) {
@@ -34,6 +35,7 @@ public record ArticleWithCommentResponse(
                 dto.createdAt(),
                 dto.userAccountDto().email(),
                 nickname,
+                dto.userAccountDto().userId(),
                 dto.articleCommentDtos().stream().map(ArticleCommentResponse::from).collect(Collectors.toCollection(LinkedHashSet::new))
         );
     }
